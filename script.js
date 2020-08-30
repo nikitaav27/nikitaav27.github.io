842bb28895b8e77bf5664d5eea1754203b53c4d0
nikitaav27 <68809816+nikitaav27@users.noreply.github.com>
2020-08-30 23:56:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ recognition.onresult = function(event) {
   // We then return the transcript property of the SpeechRecognitionAlternative object
   var color = event.results[0][0].transcript;
   diagnostic.textContent = 'Вы сказали: ' + color + '.';
-  if (color == 'система дистанционного обучения ') {
+  if (color == 'система дистанционного обучения') {
     bg.style.backgroundColor = 'green';
     qqq.textContent = 'Все верно';
     newpage();
